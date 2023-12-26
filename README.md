810e4b605b6a488d3cb985bf038d11cdc5137da7
Raymond Gan <rgan@tubi.tv>
2023-12-26 14:19:18 +0100

    Exercise 2.1: Sum of a list or range. 3 different solutions.

--- README.md
+++ README.md
@@ -4,11 +4,14 @@ I urge you buy [liveBook version](https://livebook.manning.com/book/the-little-e
 
 ## The Little Elixir & OTP Guidebook
 - [Chp 1](ch1/what_is_elixir.md): Elixir vs. Erlang. What's Elixir good/bad for? Preview of OTP behaviors.
+
+**Whirlwind Tour of Elixir:**
 - [Chp 2.2](ch2/2.2_first-steps.md): First Steps
 - [Chp 2.3-2.4](ch2/2.3-2.4_data-types_guards.md): Data Types, Guards
 - [Chp 2.5](ch2/2.5_pattern-matching.md): Pattern Matching: Destructuring. Parse audio MP3 file's ID3 tag.
 - [Chp 2.6](ch2/2.6_lists.md): Lists: Lists vs. Arrays. Flatten a list. Ordering of function clauses.
 - [Chp 2.7-2.8](ch2/2.7-2.8_pipe-operator_erlang.md): Pipe Operator. Call Erlang from Elixir. Erlang Observer.
+- [Chp 2.9](ch2/2.9_exercises.md): Exercises
 
 ## Elixir School
 - [Computer Memory](elixir_school/memory-stack_vs_heap.md): Stack vs. Heap
--- ch2/code/exercise2.1_sum.exs
+++ ch2/code/exercise2.1_sum.exs
@@ -1,9 +1,46 @@
-defmodule Ex do
+# With Enum.reduce/3: https://hexdocs.pm/elixir/Enum.html#reduce/3
+defmodule Ex0 do
   def sum(list) do
-    Enum.sum(list)
+    Enum.reduce(list, 0, fn x, acc -> acc + x end)
+  end
+end
+
+# With recursion: https://www.leighhalliday.com/recursion-in-elixir
+defmodule Ex1 do
+  def sum(list, acc \\ 0)
+
+  def sum([], acc) do
+    acc
   end
 
-  def sum(range) do
+  def sum([head | tail], acc) do
+    sum(tail, acc + head)
+  end
+
+  def sum(range, _acc) do
+    # Elixir lacks is_range() guard. All ranges are maps.
+    if range |> is_map do
+      range |> Enum.to_list() |> sum()
+    end
+  end
+end
+
+# With for/1 (list comprehension) with :reduce option
+# https://hexdocs.pm/elixir/Kernel.SpecialForms.html#for/1-the-reduce-option
+# https://hexdocs.pm/elixir/comprehensions.html
+# https://elixirschool.com/en/lessons/basics/comprehensions)
+# https://www.mitchellhanberg.com/the-comprehensive-guide-to-elixirs-for-comprehension/#reduce
+defmodule Ex2 do
+  def sum(list) do
+    for x <- list, reduce: 0 do
+      acc -> x + acc
+    end
+
+    # This returns total = 0
+
+    # total = 0
+    # for x <- list, do: total = total + x
+    # total
   end
 end
 
@@ -12,15 +49,45 @@ ExUnit.start()
 defmodule SumTest do
   use ExUnit.Case, async: true
 
-  test "sum() adds all numbers in list" do
-    assert Ex.sum([]) == 0
-    assert Ex.sum([1, 5, 10]) == 16
-    assert Ex.sum([-5, 3, 2.5]) == 0.5
+  test "sum() adds all numbers in list, with Enum.reduce/3" do
+    assert Ex0.sum([]) == 0
+    assert Ex0.sum([1, 5, 10]) == 16
+    assert Ex0.sum([-5, 3, 2.5]) == 0.5
   end
 
-  test "sum() adds all numbers in range" do
-    assert Ex.sum(0..0) == 0
-    assert Ex.sum(1..10) == 55
-    assert Ex.sum(-5..3) == -9
+  test "sum() adds all numbers in range, with Enum.reduce/3" do
+    assert Ex0.sum(0..0) == 0
+    assert Ex0.sum(1..10) == 55
+    assert Ex0.sum(-5..3) == -9
+
+    # Adds [1, 3, 5]
+    # Range syntax means first..last//step: https://hexdocs.pm/elixir/Kernel.html#..///3
+    assert Ex0.sum(1..5//2) == 9
+  end
+
+  test "sum() adds all numbers in list, with recursion" do
+    assert Ex1.sum([]) == 0
+    assert Ex1.sum([1, 5, 10]) == 16
+    assert Ex1.sum([-5, 3, 2.5]) == 0.5
+  end
+
+  test "sum() adds all numbers in range, with recursion" do
+    assert Ex1.sum(0..0) == 0
+    assert Ex1.sum(1..10) == 55
+    assert Ex1.sum(-5..3) == -9
+    assert Ex1.sum(1..5//2) == 9
+  end
+
+  test "sum() adds all numbers in list, with list comprehension" do
+    assert Ex2.sum([]) == 0
+    assert Ex2.sum([1, 5, 10]) == 16
+    assert Ex2.sum([-5, 3, 2.5]) == 0.5
+  end
+
+  test "sum() adds all numbers in range, with list comprehension" do
+    assert Ex2.sum(0..0) == 0
+    assert Ex2.sum(1..10) == 55
+    assert Ex2.sum(-5..3) == -9
+    assert Ex2.sum(1..5//2) == 9
   end
 end
