8ad932c9c327bfa5aab22e3347b39d472002551e
Raymond Gan <rgan@tubi.tv>
2024-01-06 10:34:41 +0100

    Finish Exercise 3.6: 2 ping/pong processes. Add counter. Exit after 5 ping/pongs.

--- ch3/code/exercise3.6_ping_pong.exs
+++ ch3/code/exercise3.6_ping_pong.exs
@@ -1,9 +1,19 @@
 defmodule PingReceiver do
-  def loop do
+  def loop(count \\ 0) do
     receive do
       {:ping, sender_pid} ->
-        IO.puts("Got ping. Sending pong back to #{sender_pid}.")
+        if count == 4 do
+          send(self(), :exit)
+        end
+
+        count = count + 1
+        IO.puts("Got ping. Sending pong back. Count: #{count}")
         send(sender_pid, {:pong, self()})
+        loop(count)
+
+      :exit ->
+        IO.puts("Ending after #{count} ping/pongs.")
+        Process.exit(self(), :kill)
     end
 
     loop()
@@ -14,7 +24,7 @@ defmodule PongReceiver do
   def loop do
     receive do
       {:pong, sender_pid} ->
-        IO.puts("Got pong. Sending ping back to #{sender_pid}.")
+        IO.puts("Got pong. Sending ping back.")
         send(sender_pid, {:ping, self()})
     end
 
@@ -27,8 +37,21 @@ defmodule PingPong do
     ping_pid = spawn(PingReceiver, :loop, [])
     pong_pid = spawn(PongReceiver, :loop, [])
     send(ping_pid, {:ping, pong_pid})
-    send(pong_pid, {:pong, ping_pid})
   end
 end
 
 PingPong.run()
+
+# OUTPUT:
+
+# Got ping. Sending pong back. Count: 1
+# Got pong. Sending ping back.
+# Got ping. Sending pong back. Count: 2
+# Got pong. Sending ping back.
+# Got ping. Sending pong back. Count: 3
+# Got pong. Sending ping back.
+# Got ping. Sending pong back. Count: 4
+# Got pong. Sending ping back.
+# Got ping. Sending pong back. Count: 5
+# Ending after 5 ping/pongs.
+# Got pong. Sending ping back.
